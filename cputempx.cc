00d86655491ee03c061af5c6a3013c42ee41e942
Jeff Perry <jeffsp@gmail.com>
2013-05-17 15:58:52 -0500

    add sensors version call.

--- cputempx.cc
+++ cputempx.cc
@@ -9,11 +9,17 @@
 #include <gtk/gtk.h>
 
 using namespace std;
+using namespace cputemp;
 
 int main (int argc, char *argv[])
 {
     try
     {
+        // init the sensors library
+        sensors s;
+        clog << "libsensors version " << s.get_version () << endl;
+
+        // init gtk
         GtkWidget *window;
         gtk_init (&argc, &argv);
         clog << "GTK+ version "
@@ -21,6 +27,8 @@ int main (int argc, char *argv[])
             << gtk_get_minor_version () << '.'
             << gtk_get_micro_version ()
             << endl;
+
+        // create a window
         window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
         g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
         gtk_widget_show (window);
